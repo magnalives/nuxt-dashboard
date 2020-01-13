c3b0b09154b32985044ea7a1d0b397f676ff3761
livecodebase <lunarshadeofficial@outlook.com>
2020-01-13 23:50:56 +0530

    data passage

---
+++ components/cards.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-for="card in cards" :key="card.id">
+      {{ card.id }}
+    </div>
+    <button @click="removeCard">Remove by one</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["cardData"],
+  data() {
+    return {};
+  },
+  computed: {
+    cards: function() {
+      return this.cardData;
+    }
+  },
+  watch: {
+    cards: function() {
+      if (this.cards.length < 4) {
+        this.$emit("addcards");
+      }
+    }
+  },
+  methods: {
+    removeCard() {
+      this.$emit("removecard");
+    }
+  }
+};
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,49 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        dashboard
-      </h1>
-      <h2 class="subtitle">
-        Livcodebase dashboard
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <Card
+      :cardData="cards"
+      v-on:removecard="removeCard"
+      v-on:addcards="addcards"
+    />
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import Card from "~/components/cards";
 export default {
   components: {
-    Logo
+    Card
+  },
+  data() {
+    return {
+      cards: [],
+      minCard: 0,
+      maxCard: 10
+    };
+  },
+  mounted() {
+    this.$axios.get("https://jsonplaceholder.typicode.com/todos/").then(res => {
+      this.cards = res.data.slice(this.minCard, this.maxCard);
+    });
+  },
+  methods: {
+    removeCard() {
+      this.cards = this.cards.splice(1, this.cards.length);
+    },
+    addcards() {
+      this.minCard += 7;
+      this.maxCard += 7;
+      this.$axios
+        .get("https://jsonplaceholder.typicode.com/todos/")
+        .then(res => {
+          let newcards = res.data.slice(this.minCard, this.maxCard);
+          console.log(newcards);
+
+          this.cards = newcards;
+        });
+    }
   }
-}
+};
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style scoped></style>
